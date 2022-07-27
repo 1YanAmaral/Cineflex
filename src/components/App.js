b535af25f83645692c738c05c98e4cdd2d3c1a8d
Yan Amaral <yan@pop-os.localdomain>
2022-07-27 10:08:02 -0300

    Requisição dos filmes da API

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import Header from "./Header/Header";
 import "../assets/css/style.css";
-import Movies from "./Movies/Movies";
+import MovieOptions from "./Movies/Movies";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Movies />
+      <MovieOptions />
     </>
   );
 }
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,10 +1,31 @@
 import "./style.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function Movies() {
+
+
+export default function MovieOptions() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
+    getMovies.then(movies => {setMovies(movies.data)});
+  }, []);
+
+  console.log(movies)
+
   return (
     <div className="page">
       <div className="title">Selecione o filme</div>
-      <div className="screening"></div>
+      <div className="screening">
+        {movies.map((poster) => (
+          <div className="poster">
+            <img src={poster.posterURL} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
