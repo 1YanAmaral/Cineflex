2f84f5080d6d9fc4929acc268bcd3520aa02fcb4
Yan Amaral <yan@pop-os.localdomain>
2022-07-27 09:27:37 -0300

    Layout tela inicial

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Header from "./Header/Header";
+import "../assets/css/style.css";
+import Movies from "./Movies/Movies";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Movies />
+    </>
+  );
+}
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+export default function Movies() {
+  return (
+    <div className="page">
+      <div className="title">Selecione o filme</div>
+      <div className="screening"></div>
+    </div>
+  );
+}
