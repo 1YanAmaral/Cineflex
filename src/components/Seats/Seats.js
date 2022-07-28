9cfca2f51db6c18030e0c36989eb90d132509b27
Yan Amaral <yan@pop-os.localdomain>
2022-07-28 12:26:42 -0300

    Layout tela de escolha de assentos

--- src/components/Seats/Seats.js
+++ src/components/Seats/Seats.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 export default function Seats() {
+  const [selectedMovie, setSelectedMovie] = useState({});
   const [seats, setSeats] = useState([]);
   const { sessionId } = useParams();
 
@@ -17,18 +18,49 @@ export default function Seats() {
       setSeats(seats.data.seats);
     });
   }, []);
+
+  //   useEffect(() => { Vai pro Footer
+  //     const getSelectedMovie = axios.get(
+  //       `https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${sessionId}/seats`
+  //     );
+
+  //     getSelectedMovie.then((movie) => {
+  //       setSelectedMovie(movie.data);
+  //     });
+  //   }, []);
   console.log(seats);
 
   return (
     <div className="page">
       <div className="title">Selecione o(s) assento(s)</div>
-      {seats.map((seat) =>
-        seat.isAvailiable ? (
-          <div></div>
-        ) : (
-          <div className="poster">{seat.name}</div>
-        )
-      )}
+      <div className="seats">
+        {seats.map((seat) =>
+          seat.isAvailiable ? (
+            <div className="seat">{seat.name}</div>
+          ) : (
+            <div className="seat unavailiable">{seat.name}</div>
+          )
+        )}
+      </div>
+      <div className="colorcode">
+        <div className="code">
+          <div className="seat selected"></div>
+          Selecionado
+        </div>
+        <div className="code">
+          <div className="seat"></div>
+          Disponível
+        </div>
+        <div className="code">
+          <div className="seat unavailiable"></div>
+          Indisponível
+        </div>
+      </div>
+      <div className="buyer">Nome do comprador:</div>
+      <input placeholder="Digite seu nome..."></input>
+      <div className="buyer">CPF do comprador:</div>
+      <input placeholder="Digite seu CPF..."></input>
+      <button>Reservar assento(s)</button>
     </div>
   );
 }
